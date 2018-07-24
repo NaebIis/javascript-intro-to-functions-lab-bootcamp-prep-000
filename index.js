f1b1640b6319a1d199f1c51d84221be8f7468f5e
John Grubbs <johnmlowery@gmail.com>
2018-07-24 17:11:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function sayHiToGrandma(string){
     return ("I can\'t hear you!")
 }
  else if (string === string.toUpperCase())
- return ("YES INDEED!") 
+     return ("YES INDEED!") 
  
  else if (string === string.toLowerCase())
- return ("I love you, too.")
+    return ("I love you, too.")
 }
   
