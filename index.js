38bc617e1adaed8d007ac2339a272b19a56301d0
John Grubbs <johnmlowery@gmail.com>
2018-07-24 17:00:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,5 @@ function logShout(string) {
   console.log(string.toUpperCase())}
 function logWhisper(string) { 
   console.log(string.toLowerCase())}
-function sayHiToGrandma(string) {
-  if (string.toUpperCase())
-  return string.toLowerCase() }
-
-
-
-
-
-function sayHiToGrandma(string) {
-  return string.toUpperCase() }
-function sayHiToGrandma(string) {
-  return string.toLowerCase() }
+function sayHiToGrandma(string) { 
+  if 
