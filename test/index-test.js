de6c0697c69afc410118f919783667508d55b221
John Grubbs <johnmlowery@gmail.com>
2018-07-24 16:41:40 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -34,7 +34,7 @@ describe('logWhisper(string)', function() {
   })
 })
 
-describe('sayHiToGrandma1(string)', function() {
+describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
